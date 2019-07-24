9ce85e13d0d6e8f4c00e6ba1d6fbca082718c6be
sebasmontoyasm <sebasmontoyasm@gmail.com>
2019-07-23 22:29:28 -0500

    Revert "Correcion de instalacion, actualizacion de Nugets y Ejemplo funcional en celular fisico"
    This reverts commit 526ffb951a04116ca19fc00c3f90dcdb5045cb09.

---
+++ ProyectCompraExpress/ProyectCompraExpress/ProyectCompraExpress/ConfirmarCompraPage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ProyectCompraExpress
+{
+    class ConfirmarCompraPage : ContentPage 
+    {
+        public ConfirmarCompraPage()
+        {
+            var lbl1 = new Label
+            {
+                Text = "Escribre tu nombre"
+
+            };
+
+            var txtNombre = new Entry
+            {
+                Placeholder = "Escribe tu nombre"
+            };
+            Content = new StackLayout
+            {
+                Padding = 30,
+                Spacing = 10,
+                Children = { lbl1, txtNombre }
+            };
+    }
+}
