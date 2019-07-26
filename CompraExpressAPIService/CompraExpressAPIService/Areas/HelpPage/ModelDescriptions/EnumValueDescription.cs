873ef1ae5c7d71c650b757ff1f3b9cd74acaa0a0
sebasmontoyasm <sebasmontoyasm@gmail.com>
2019-07-25 20:23:24 -0500

    Servicio WebAPI creado no funcional, Confirmar compra Case Fronend actualizado

---
+++ CompraExpressAPIService/CompraExpressAPIService/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace CompraExpressAPIService.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
