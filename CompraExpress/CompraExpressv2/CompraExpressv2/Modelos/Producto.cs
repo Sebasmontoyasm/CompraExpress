e328b0e9bdbab4d2bf28f224de6e5515fca88b28
sebasmontoyasm <sebasmontoyasm@gmail.com>
2019-07-25 21:00:49 -0500

    Servicio WebAPI creado no funcional, Confirmar Compra case Fronend actualizado DB.

---
+++ CompraExpress/CompraExpressv2/CompraExpressv2/Modelos/Producto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompraExpressv2.Modelos
+{
+    public class Producto
+    {
+        public string id { get; set; }
+        public string nombre { get;set; }
+        public string descripcion { get;set; }
+        public int Precio { get; set; }
+
+        public Producto(string newid,string nombre,string newdescripcion,int precio)
+        {
+            this.id = newid;
+            this.nombre = nombre;
+            this.descripcion = newdescripcion;
+            this.Precio = precio;
+        }
+    }
+}
