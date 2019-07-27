6ff6b9b23b0647605609f96b19c1b590e52676b9
sebasmontoyasm <sebasmontoyasm@gmail.com>
2019-07-26 19:56:36 -0500

    Servicio WebAPI creado no funcional, Confirmar Compra case Fronend actualizada bd

---
+++ CompraExpress/CompraExpressv2/CompraExpressv2/Modelos/Empresa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompraExpressv2.Modelos
+{
+    public class Empresa
+    {
+        public string id { get; set; }
+        public string Nombre { get; set; }
+
+        public string nCuenta { get; set; }
+
+        public Empresa(string newid,string nombre,string ncuenta)
+        {
+            this.id = newid;
+            this.Nombre = nombre;
+            this.nCuenta = ncuenta;
+        }
+    }
+}
---
+++ CompraExpress/CompraExpressv2/CompraExpressv2/Views/ConfirmarCompraPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using CompraExpressv2.Modelos;
+
+namespace CompraExpressv2.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConfirmarCompraPage : ContentPage
+    {
+        public Cliente cliente { get; set; }
+        public Empresa empresa { get; set; }
+        public Compra compra { get; set; }
+        public Producto producto { get; set; }
+        public const double IVA = 0.7;
+    
+        public ConfirmarCompraPage()
+        {
+            this.cliente = new Cliente(1053854498,"Sebastian Montoya", "Cra 36a #10D-66","Manizales","Colombia","sebasmontoyasm@gmail.com");
+            this.empresa = new Empresa("1701410810","Ktronix", "6016 6071 9235 4142");
+            this.producto = new Producto("13045876", "Portatil Gamer HP", "Procesador i5 " +
+                                                                    "SO: Windows Home " +
+                                                                    "Memoria ram: 8GB " +
+                                                                    "Disco duro: 1 TB " +
+                                                                    "Pantalla 15.6 pulgadas" +
+                                                                    "Tarjeta Grafica: NVIDIA ", 224900);
+            LinkedList<Producto> productos = new LinkedList<Producto>();
+            productos.Append(producto);
+
+            int precioTotal = 0;
+            foreach(Producto p in productos)
+            {
+                precioTotal = precioTotal + p.Precio;
+            }
+
+
+            this.compra = new Compra("85698524539", cliente.Documento,empresa.id,productos,precioTotal + precioTotal*IVA,DateTime.Now);
+
+
+            BindingContext = this;
+
+			InitializeComponent ();
+		}
+
+        private
+    }
+}
